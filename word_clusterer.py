367a49d75db149df46c98f771e7a0881d67c3414
Andy Le <andy.quile@gmail.com>
2022-11-30 12:00:00 -0800

    Create program word_clusterer.py

---
+++ word_clusterer.py
@@ -0,0 +1,19 @@
+# Word Clusterer
+
+# Import libraries
+import glob
+import os.path
+import sys
+
+# Main method
+def main():
+	# Retrieve user arguments 
+	# provided to the Python program
+    args = sys.argv[1:]
+
+    
+
+
+# Execute script
+if __name__ == "__main__":
+	main()
